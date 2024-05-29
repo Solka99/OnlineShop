f5d843001f46c1ab471fb9b7070982c339aa08f4
Solka99 <alicjas508@gmail.com>
2024-05-29 13:33:00 +0200

    Zapisywanie do bazy

---
+++ OnlineShop/OnlineShopSeeder.cs
@@ -0,0 +1,60 @@
+﻿using OnlineShop.Models;
+using System.Collections.Specialized;
+
+namespace OnlineShop
+{
+    public class OnlineShopSeeder
+    {
+        private readonly MyDbContext _myDbContext;
+
+        public OnlineShopSeeder(MyDbContext myDbContext)
+        {
+            _myDbContext = myDbContext;
+        }
+        public void Seed()
+        {
+            if (_myDbContext.Database.CanConnect())
+            {
+                if(!_myDbContext.Products.Any()) 
+                {
+                    var categories=GetCategories();
+                    var products = GetProducts();
+                    _myDbContext.AddRange(categories);
+                    _myDbContext.AddRange(products);
+                    if (_myDbContext.SaveChanges() > 0)
+                    {
+                        Console.WriteLine("Sukces");
+                    }
+                    else { Console.WriteLine("Porażka"); }
+                }
+            }
+        }
+        private IEnumerable<Product> GetProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product()
+                {
+                    Name="Coca Cola",
+                    Description = "Very yummy drink",
+                    Price=3,
+                    CategoryID=1,
+                    Stock=300
+                },
+                
+            };
+            return products;
+
+        }
+        private IEnumerable<Category> GetCategories()
+        {
+            var categories = new List<Category>()
+            {
+                new Category() {
+                    Name = "Drink"
+                }
+            };
+            return categories;
+        }
+    }
+}
--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MyDbContext>();
+builder.Services.AddScoped<OnlineShopSeeder>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,5 +27,22 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<MyDbContext>();
+        context.Database.Migrate(); // Ensure the database is created and migrations are applied
+        OnlineShopSeeder seeder=new OnlineShopSeeder(context);
+        seeder.Seed(); // Call the seed method
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the database.");
+    }
+}
 
 app.Run();
